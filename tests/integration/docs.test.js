6520e35ee7f9e4905c337aa9fdcf6a18064efd2f
Victor Miti <victormiti@umusebo.com>
2020-12-25 19:12:07 +0200

    refactor the test
    inspired by hagopj13/node-express-mongoose-boilerplate@dd8bfd7

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -2,24 +2,21 @@ const request = require('supertest');
 const httpStatus = require('http-status');
 const config = require('../../src/config/config');
 
-describe('availability of /docs', () => {
+describe('GET /v1/docs', () => {
   afterEach(() => {
     process.env.NODE_ENV = 'test';
     config.env = process.env.NODE_ENV;
     jest.resetModules();
   });
   const envs = [
-    // [env, docs, status]
-    ['test', 'available', httpStatus.OK],
-    ['development', 'available', httpStatus.OK],
-    ['production', 'unavailable', httpStatus.NOT_FOUND],
+    // [env, status]
+    ['test', httpStatus.OK],
+    ['development', httpStatus.OK],
+    ['production', httpStatus.NOT_FOUND],
   ];
-  test.each(envs)('in %p mode, /docs should be %p', async (env, docs, status) => {
+  test.each(envs)('when running in %p, should return %i', async (env, status) => {
     process.env.NODE_ENV = env;
-    const routes = require('../../src/routes/v1'); // eslint-disable-line global-require
     const app = require('../../src/app'); // eslint-disable-line global-require
-    // v1 api routes
-    app.use('/v1', routes);
-    await request(app).get('/v1/docs/').send().expect(status);
+    await request(app).get('/v1/docs/').expect(status);
   });
 });
